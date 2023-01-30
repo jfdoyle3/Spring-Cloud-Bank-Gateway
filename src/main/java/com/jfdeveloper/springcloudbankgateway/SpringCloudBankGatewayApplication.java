3e581c9ffee0cc2044e91d6f1b1c0c1b8ce20dab
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-01-29 21:44:06 -0500

    Created Project

---
+++ src/main/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplication.java
@@ -0,0 +1,41 @@
+package com.jfdeveloper.springcloudbankgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringCloudBankGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudBankGatewayApplication.class, args);
+	}
+
+	// Simple Route
+//	@Bean
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route(p -> p
+//						.path("/get")
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+//						.uri("http://httpbin.org:80"))
+//				.build();
+//	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.uri("http://httpbin.org:80")).
+				build();
+	}
+
+}
---
+++ src/test/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfdeveloper.springcloudbankgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBankGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
