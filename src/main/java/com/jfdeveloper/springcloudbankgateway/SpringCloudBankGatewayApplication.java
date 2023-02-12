47170474ec5f641f581f576c3d433743348918ef
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-02-11 20:02:00 -0500

    Trying to get the gateway working

--- src/main/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplication.java
+++ src/main/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplication.java
@@ -2,6 +2,7 @@ package com.jfdeveloper.springcloudbankgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudBankGatewayApplication {
 
 	public static void main(String[] args) {
