0cb767a11057afc7e7fecde8e4343f8be6ab4a44
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-02-13 21:08:43 -0500

    Trying to get the gateway working with Eureka

--- src/main/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplication.java
+++ src/main/java/com/jfdeveloper/springcloudbankgateway/SpringCloudBankGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -15,11 +16,13 @@ import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class SpringCloudBankGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringCloudBankGatewayApplication.class, args);
+
 	}
 
 	// Simple Route
@@ -33,37 +36,37 @@ public class SpringCloudBankGatewayApplication {
 //				.build();
 //	}
 
-    @Component
-    public class AddHeaderFilter implements GatewayFilter {
+ //   @Component
+ //   public class AddHeaderFilter implements GatewayFilter {
 
-        @Override
-        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-            ServerHttpRequest request = exchange.getRequest().mutate()
-                    .header("header-name", "header-value")
-                    .build();
-
-            ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
-            return chain.filter(mutatedExchange);
-        }
-    }
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-
-				.route(p -> p
-						.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80"))
-				.route(p -> p
-						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
-						.uri("http://httpbin.org:80"))
-                .route("create", r -> r.path("/api/**")
-                        .filters(f -> f.filter(new AddHeaderFilter()))
-                        .uri("http://localhost:8080"))
-				.build();
+//        @Override
+//        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//            ServerHttpRequest request = exchange.getRequest().mutate()
+//                    .header("header-name", "header-value")
+//                    .build();
+//
+//            ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
+//            return chain.filter(mutatedExchange);
+//        }
+//    }
+//	@Bean
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//
+//				.route(p -> p
+//						.path("/get")
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+//						.uri("http://httpbin.org:80"))
+//				.route(p -> p
+//						.host("*.circuitbreaker.com")
+//						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+//						.uri("http://httpbin.org:80"))
+//                .route("create", r -> r.path("/api/**")
+//                        .filters(f -> f.filter(new AddHeaderFilter()))
+//                        .uri("http://localhost:8080"))
+//				.build();
 	}
 
 
 
-}
+//}
